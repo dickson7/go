091f0c8afad330b1d63c520d705966248f1bfacc
dickson7 <garciadickson@hotmail.com>
2019-03-10 20:49:57 -0500

    03_ constamtes y valor cero de variables

---
+++ 03_constantes/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	/*
+	   El valor de una constante no
+	   se pueden cambiar
+	*/
+	//Comentarios de linea para documentar codigo
+
+	const nombre = "Dickson"
+
+	fmt.Println(nombre)
+
+	//valor cero de variables
+	var nomb string
+	var numero int
+	var entendes bool
+	fmt.Println(nomb, numero, entendes)
+}
